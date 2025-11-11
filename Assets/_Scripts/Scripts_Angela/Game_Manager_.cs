0ba909b75ae996cc05bd05430be6ea9e267b613a
nereaseixas7 <nerea.seixas.7@gmail.com>
2025-11-11 14:33:36 +0100

    inicio y música

--- Assets/_Scripts/Scripts_Angela/Game_Manager_.cs
+++ Assets/_Scripts/Scripts_Angela/Game_Manager_.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class Game_Manager_ : MonoBehaviour
 {
-    [SerializeField] int puntuacionActual
+    [SerializeField] int puntuacionActual;
     [SerializeField] int puntuacionMaxima;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
